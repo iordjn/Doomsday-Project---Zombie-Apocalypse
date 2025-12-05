3646bc614969a3cc4551253f5561760a02c058bc
Jordin <jordin.chavez@my.sctcc.edu>
2025-12-04 19:31:11 -0600

    Create zombie_db_tables.sql

---
+++ zombie_db_tables.sql
@@ -0,0 +1,59 @@
+﻿/* Survivors – Arlo */
+
+
+
+
+/* MissionSurvivors (link) -- Arlo */
+
+
+
+
+/* SurvivorsInjuries (link) – Arlo */
+
+
+
+
+
+/* Missions – Ashton */
+
+
+
+
+/* Allies – Ashton */
+
+
+
+
+/* Weapons – Arlo */
+
+
+
+
+/* Food – Alex */
+
+
+
+
+/* Water – Alex */
+
+
+
+
+/* Injuries – Jordin */
+
+
+
+
+/* Base – Jordin */
+
+
+
+
+/* Vehicles – Muse */
+
+
+
+
+/* Supplies – Muse */
+
+
