d158a4bca27bdd0e5ea9f87abc95c7745f60bccb
Ashton Fox <116043127+afox00001@users.noreply.github.com>
2025-12-05 19:15:01 -0600

    Update zombie_db_tables.sql

--- zombie_db_tables.sql
+++ zombie_db_tables.sql
@@ -58,12 +58,28 @@ GO
 
 
 /* Food – Alex */
-
+Create Table Food
+(
+  food_id   INT      NOT NULL,
+  food_name VARCHAR(80)  NOT NULL,
+  category       VARCHAR(40),
+  calories_per_unit     INT,
+  shelf_life_days      INT,
+  CONSTRAINT pk_Food PRIMARY KEY (food_id)
+  );
 
 
 
 /* Water – Alex */
-
+Create Table Water
+(
+  water_id   INT      NOT NULL,
+  source_type VARCHAR(40)  NOT NULL,
+  container_type      VARCHAR(40),
+  volume_liters     DECIMAL(10,2),
+  is_purified      BIT,
+  CONSTRAINT pk_Water PRIMARY KEY (water_id)
+  );
 
 
 
